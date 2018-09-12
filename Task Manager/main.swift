bd60f55e4dbf71688b06e038408944e74999007f
Solomon Keiffer <solomonkeiffer@macbook-pro-80.local>
2018-09-12 10:04:55 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Solomon Keiffer on 9/12/18.
+//  Copyright © 2018 Phoenix Development. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
