adb8eea322c4126182ba7d582057e49766fa27e1
Solomon Keiffer <solomonkeiffer@macbook-pro-80.local>
2018-09-12 10:11:14 -0500

    Initial Commit.

---
+++ Task Manager/Task.swift
@@ -0,0 +1,19 @@
+//  Task.swift
+//  Task Manager
+//
+//  Created by Solomon Kieffer on 9/12/18.
+//  Copyright © 2018 Phoenix Development. All rights reserved.
+
+import Foundation
+
+class Task {
+    var Name: String
+    var Description: String
+    var Status: Bool
+    var finishDate: Date
+    
+    init(Name: String, Description: String, timeToComplete finishBy: String) {
+        <#statements#>
+    }
+
+}
--- Task Manager/main.swift
+++ Task Manager/main.swift
@@ -1,12 +1,7 @@
-//
 //  main.swift
 //  Task Manager
 //
-//  Created by Solomon Keiffer on 9/12/18.
+//  Created by Solomon Kieffer on 9/12/18.
 //  Copyright © 2018 Phoenix Development. All rights reserved.
-//
 
-import Foundation
-
-print("Hello, World!")
 
